80e24cf65598c99eba2b1defb5efc714660dc3a3
gpowers01 <gpowers01@users.noreply.github.com>
2015-04-22 06:37:46 -0500

    Update server.js
    Add some simple debug messages and client connection counts to do something besides hello world.

--- server.js
+++ server.js
@@ -2,7 +2,12 @@
 
 var http = require('http')
 var port = process.env.PORT || 1337;
+console.log('Starting up - port is:' + port);
+var resCount = 0;
+
 http.createServer(function(req, res) {
+  console.log('Response count:' + ++resCount);
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('Hello World\n');
+  console.log('Sent response to client.');
 }).listen(port);
